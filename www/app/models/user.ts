b7c14cb6ea42e125aef9e85316f5018dcaa352e6
Diego Henrique Domingues <diego.domingues16@gmail.com>
2015-11-11 12:53:22 -0200

    Add user model

---
+++ www/app/models/user.ts
@@ -0,0 +1,20 @@
+export class User {
+    id:number;
+    name:string;
+    email:string;
+    integration_id:string;
+    integration_type:number;
+    bio:string;
+    birthday:string;
+
+    constructor() {
+    }
+
+    avatar() {
+        if (this.integration_type === 1) {
+            return `https://graph.facebook.com/${this.integration_id}/picture?width=400&height=400`;
+        } else {
+            return null;
+        }
+    }
+}
