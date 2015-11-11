663945c159207e5de601b27b071138dda8d2dc6f
Diego Henrique Domingues <diego.domingues16@gmail.com>
2015-11-11 13:40:10 -0200

    Sign up the user after facebook authentication
    Now when the user authenticates on facebook it is verified if he exists on API. If exists use the user of API otherwise sign up the user and then redirect the user to profile

--- www/app/authentication/authentication.js
+++ www/app/authentication/authentication.js
@@ -1,25 +1,74 @@
+'use strict';
+
 import {Page, Events} from 'ionic/ionic';
 import {FacebookService} from './../services/facebook'
+import {UsersService} from './../services/users'
 
 @Page({
   templateUrl: 'app/authentication/authentication.html'
 })
 export class AuthenticationPage {
-  constructor(events: Events, facebookService: FacebookService) {
+  constructor(events:Events, facebookService:FacebookService, users:UsersService) {
     this.facebookService = facebookService;
     this.events = events;
+    this.users = users;
   }
 
   login() {
-    let self = this;
-    this.facebookService.login({scope: 'email,publish_actions'}).then(() => {
-      self.facebookService.api({
+    this.facebookService
+      .login({scope: 'email,publish_actions'})
+      .then(() => this.getFacebookUser());
+  }
+
+  getFacebookUser() {
+    this.facebookService
+      .api({
         path: '/me',
         params: {fields: 'id,name,email'}
-      }).then(
-        (user) => self.events.publish('user:authenticated', user),
-        (error) => alert('Facebook error: ' + error.error_description)
+      })
+      .then(
+        (user) => this.verifyIfUserExistsOnApi(user),
+        (error) => console.log('Facebook error: ', JSON.stringify(error))
       );
-    });
+  }
+
+  verifyIfUserExistsOnApi(user) {
+    let userFromFacebook = (u) => u.integration_type === 1 && u.integration_id === user.id;
+
+    this.users
+      .all()
+      .subscribe(
+        (response) => {
+          let userFromApi = response.users.find(userFromFacebook);
+
+          if (!userFromApi) {
+            this.signUpUser(user);
+          } else {
+            this.notifyWith(userFromApi);
+          }
+        }
+      );
+  }
+
+  signUpUser(user) {
+    this.users
+      .create(
+        {
+          "user": {
+            "name": user.name,
+            "email": user.email,
+            "integration_type": 1,
+            "integration_id": user.id
+          }
+        }
+      )
+      .subscribe(
+        (response) => this.notifyWith(response.user),
+        (error) => console.log('erro ao criar o usuário', user, JSON.stringify(error))
+      );
+  }
+
+  notifyWith(user) {
+    this.events.publish('user:authenticated', user);
   }
 }
