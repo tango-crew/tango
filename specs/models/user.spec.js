b16d5adca5c6162bff7c0d8a357449223101682d
Diego Henrique Domingues <diego.domingues16@gmail.com>
2015-11-25 00:30:57 -0200

    Add User spec

---
+++ specs/models/user.spec.js
@@ -0,0 +1,16 @@
+import {User} from './../../www/app/models/user'
+
+describe('User', () => {
+  describe('#avatar', () => {
+
+    describe('when it is a facebook integration', () => {
+      var user = new User();
+      user.integration_type = 1;
+      user.integration_id = 2;
+
+      it('returns a facebook url', function() {
+        expect(user.avatar()).toMatch(/graph\.facebook\.com\/2/);
+      });
+    });
+  });
+});
