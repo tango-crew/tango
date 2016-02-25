2641cf993f68831a3bb2c80e2e1ac8e22161edff
Diego Henrique Domingues <diego.domingues16@gmail.com>
2016-02-25 01:30:11 -0300

    Adding missing properties to User

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,20 +1,22 @@
 export class User {
-    id:number;
-    name:string;
-    email:string;
-    integration_id:string;
-    integration_type:number;
-    bio:string;
-    birthday:string;
+  id:number;
+  name:string;
+  email:string;
+  integration_id:string;
+  integration_type:number;
+  bio:string;
+  birthday:string;
+  password:string;
+  password_confirmation:string;
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    avatar() {
-        if (this.integration_type === 1) {
-            return `https://graph.facebook.com/${this.integration_id}/picture?width=400&height=400`;
-        } else {
-            return null;
-        }
+  avatar() {
+    if (this.integration_type === 1) {
+      return `https://graph.facebook.com/${this.integration_id}/picture?width=400&height=400`;
+    } else {
+      return null;
     }
+  }
 }
