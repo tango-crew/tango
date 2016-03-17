0348558a7e77a81e84b15ad95bab98c8b8addb42
Diego Henrique Domingues <diego.domingues16@gmail.com>
2016-03-17 18:36:27 -0300

    Enabling profile edition from profile page

--- app/pages/profile/profile.ts
+++ app/pages/profile/profile.ts
@@ -1,27 +1,26 @@
-import {Page, Storage, LocalStorage} from 'ionic-framework/ionic';
+import {Page, Storage, LocalStorage, NavController} from 'ionic-framework/ionic';
 import {Camera} from 'ionic-native';
 import {User} from '../../models/user';
+import {ProfileEditPage} from '../profile_edit/profile_edit';
+import {AmazonS3Service} from '../../services/amazon_s3';
 
 @Page({
   templateUrl: 'build/pages/profile/profile.html'
 })
 export class ProfilePage {
-  user: User;
+  user:User;
+  image_url:string;
 
-  constructor() {
-    this.initialize();
-  }
-
-  initialize() {
+  constructor(private navController:NavController, private amazonS3Service:AmazonS3Service) {
     new Storage(LocalStorage)
       .get('user')
-      .then((user) => this.user = Object.assign(new User(), JSON.parse(user)));
+      .then(user => {
+        this.user = Object.assign(new User(), JSON.parse(user));
+        this.image_url = amazonS3Service.getSignedImageUrl(this.user.profile_image_id);
+      });
   }
 
-  choosePhoto() {
-    Camera.getPicture().then(
-      imageData => this.user.image_url = imageData,
-      err => alert(err)
-    );
+  edit() {
+    this.navController.push(ProfileEditPage, {user: this.user});
   }
 }
