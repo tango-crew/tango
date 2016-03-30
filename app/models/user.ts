4155c48783d58e57ccce56eb9bce27064ef68b04
Diego Henrique Domingues <diego.domingues16@gmail.com>
2016-03-30 01:14:00 -0300

    Fixing the user model

--- app/models/user.ts
+++ app/models/user.ts
@@ -11,7 +11,7 @@ export class User {
   profile_image_id:string;
   profile_image_filename:string;
   profile_image_content_type:string;
-  profile_image_size:integer;
+  profile_image_size:number;
   remote_profile_image_url:string;/* Used only to upload image */
 
   constructor() {
@@ -21,7 +21,7 @@ export class User {
     if (this.integration_type === 1) {
       return `https://graph.facebook.com/${this.integration_id}/picture?width=400&height=400`;
     } else {
-      return this.image_url || 'img/avatar.png';
+      return 'img/avatar.png';
     }
   }
 }
