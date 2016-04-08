78ade853c0d32a5c37f1a43ed8455f6da68636fb
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2016-04-08 12:35:07 -0300

    test(test): Adding user feature
    Adds cucumber user feature;
    Adds cucumber steps definitions;
    Adds id on `sign-up` a-href element.

---
+++ features/step_definitions/user_steps.js
@@ -0,0 +1,41 @@
+module.exports = function () {
+  this.Given(/^I am not authenticated$/, function () {
+    browser.get('');
+  });
+
+  this.When(/^I go to register$/, function (callback) {
+    element(by.css('#sign-up')).click();
+    
+    setTimeout(function () {
+      var nameField = element(by.css('#user-name input'));
+      var emailField = element(by.css('#user-email input'));
+      var passwordField = element(by.css('#user-password input'));
+      var passwordConfirmationField = element(by.css('#user-password-confirmation input'));
+      var signInButton = element(by.css('#sign-up-form button'));
+
+      nameField.sendKeys('Diego gatinho');
+      emailField.sendKeys('test.diego@gmail.com');
+      passwordField.sendKeys('123');
+      passwordConfirmationField.sendKeys('123');
+
+      signInButton.click();
+
+      callback();
+    }, 1000);
+  });
+
+  this.When(/^I fill the form with some valid data$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.When(/^I press "([^"]*)"$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Then(/^I should see "([^"]*)"$/, function (arg1, email, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+};
