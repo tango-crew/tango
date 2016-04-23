f29d307a840ca2cc14ace7ac9232598567345c73
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2016-04-23 16:53:59 -0300

    chore(test): Add cucumber library
    Add cucumber library and acceptance tests architecture

---
+++ config/protractor.conf.js
@@ -0,0 +1,44 @@
+// @AngularClass
+require('ts-node/register');
+var helpers = require('./helpers');
+
+exports.config = {
+  /**
+   * Angular 2 configuration
+   *
+   * useAllAngular2AppRoots: tells Protractor to wait for any angular2 apps on the page instead of just the one matching
+   * `rootEl`
+   *
+   */
+  useAllAngular2AppRoots: true,
+
+  /* LOCALHOST CONFIG */
+  seleniumServerJar: "node_modules/protractor/selenium/selenium-server-standalone-2.52.0.jar",
+  baseUrl: 'http://localhost:8100',
+
+  exclude: [],
+
+  allScriptsTimeout: 110000,
+
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  specs: [
+    helpers.root('test/features/**/*.feature')
+  ],
+  cucumberOpts: {
+    require: [
+      'test/features/step_definitions/**/*.steps.ts'
+    ],
+    format: 'pretty'
+  },
+
+  directConnect: true,
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  onPrepare: function() {
+    browser.ignoreSynchronization = false;
+  }
+};
